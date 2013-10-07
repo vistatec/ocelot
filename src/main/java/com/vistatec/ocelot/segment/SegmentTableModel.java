b5ec0a04028f8d3605d89f369a5dbe57570ae5eb
Kevin Lew <kevin@spartansoftwareinc.com>
2013-10-07 14:44:37 -0700

    LGPL v3 license

--- src/main/java/com/vistatec/ocelot/segment/SegmentTableModel.java
+++ src/main/java/com/vistatec/ocelot/segment/SegmentTableModel.java
@@ -1,3 +1,31 @@
+/*
+ * Copyright (C) 2013, VistaTEC or third-party contributors as indicated
+ * by the @author tags or express copyright attribution statements applied by
+ * the authors. All third-party contributions are distributed under license by
+ * VistaTEC.
+ *
+ * This file is part of Ocelot.
+ *
+ * Ocelot is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Lesser General Public License as published by
+ * the Free Software Foundation, either version 3 of the License, or
+ * (at your option) any later version.
+ *
+ * Ocelot is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Lesser General Public License for more details.
+ *
+ * You should have received a copy of the GNU Lesser General Public License
+ * along with this program. If not, write to:
+ *
+ *     Free Software Foundation, Inc.
+ *     51 Franklin Street, Fifth Floor
+ *     Boston, MA 02110-1301
+ *     USA
+ *
+ * Also, see the full LGPL text here: <http://www.gnu.org/copyleft/lesser.html>
+ */
 package com.vistatec.ocelot.segment;
 
 import com.vistatec.ocelot.rules.DataCategoryFlag;
