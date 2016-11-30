4b44fe2280fd6e4a4381412bcd842fca0956cf66
Marta Borriello <Marta.Borriello@vistatec.com>
2016-11-30 10:43:29 +0000

    Change tracking module revisions element pointing to the correct segment - Fixes Jira issue OC-87

--- src/main/java/com/vistatec/ocelot/xliff/okapi/OkapiXLIFF20Parser.java
+++ src/main/java/com/vistatec/ocelot/xliff/okapi/OkapiXLIFF20Parser.java
@@ -88,7 +88,8 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 	        DATETIME_PATTERN);
 	private List<Event> events;
 	private List<net.sf.okapi.lib.xliff2.core.Segment> segmentUnitParts;
-	private List<TargetVersion> targetVersions;
+//	private List<TargetVersion> targetVersions;
+	private Map<String, TargetVersion> targetVersions;
 	private Map<Integer, Integer> segmentEventMapping;
 	private int documentSegmentNum;
 	private String sourceLang, targetLang;
@@ -107,9 +108,12 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 		return this.segmentUnitParts.get(segmentUnitPartIndex);
 	}
 	
-	public TargetVersion getTargetVersion(int segmentUnitPartIndex){
-		return this.targetVersions.get(segmentUnitPartIndex);
+	public TargetVersion getTargetVersion(String segmentRef){
+		return this.targetVersions.get(segmentRef);
 	}
+//	public TargetVersion getTargetVersion(int segmentUnitPartIndex){
+//		return this.targetVersions.get(segmentUnitPartIndex);
+//	}
 
 	@Override
 	public List<OcelotSegment> parse(File xliffFile) throws IOException {
@@ -117,7 +121,8 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 		segmentEventMapping = new HashMap<Integer, Integer>();
 		events = new LinkedList<Event>();
 		segmentUnitParts = new LinkedList<>();
-		targetVersions = new ArrayList<TargetVersion>();
+//		targetVersions = new ArrayList<TargetVersion>();
+		targetVersions = new HashMap<String, TargetVersion>();
 		this.documentSegmentNum = 1;
 		int segmentUnitPartIndex = 0;
 
@@ -150,7 +155,7 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 						net.sf.okapi.lib.xliff2.core.Segment okapiSegment =
                                 (net.sf.okapi.lib.xliff2.core.Segment) unitPart;
 						OcelotSegment ocelotSegment = convertPartToSegment(
-						        okapiSegment, segmentUnitPartIndex++, sourceEnrichments, targetEnrichments, unit.getId());
+						        okapiSegment, segmentUnitPartIndex++, sourceEnrichments, targetEnrichments, unit.getId(), unitPart.getId(true));
 						if (ocelotSegment.getTarget() != null) {
 							setTargetRevisions(unit, okapiSegment,
 							        ocelotSegment);
@@ -204,7 +209,7 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 			Revisions revs = null;
 			while (revsIt.hasNext()) {
 				revs = revsIt.next();
-				if (revs.getAppliesTo().equals(Const.ELEM_TARGET)) {
+				if (isRevisionsForTarget(revs, ocelotSegment.getSegmentId())) {
 					targetRevisions = revs;
 					break;
 				}
@@ -224,7 +229,7 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 				}
 			} else if (!ocelotSegment.getTarget().getDisplayText().isEmpty()) {
 				targetRevisions = createRevisionsForTarget(okapiSegment
-				        .getTarget());
+				        .getTarget(), ocelotSegment.getSegmentId());
 				unit.getChangeTrack().add(targetRevisions);
 				ocelotRevisions.add(new OcelotRevision(targetRevisions.get(0),
 				        targetRevisions.get(0).get(0)));
@@ -268,29 +273,37 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 					targetRevisions.add(revision);
 					targetRevisions.setCurrentVersion(revision.getVersion());
 				}
-				targetVersions.add(new TargetVersion(TargetVersion.VERSION_PREFIX + nextVersion));
+				targetVersions.put(ocelotSegment.getSegmentId(), new TargetVersion(TargetVersion.VERSION_PREFIX + nextVersion));
 			} else {
-				targetVersions.add(new TargetVersion(TargetVersion.VERSION_PREFIX + "1"));
+				targetVersions.put(ocelotSegment.getSegmentId(), new TargetVersion(TargetVersion.VERSION_PREFIX + "1"));
 			}
 
 		} else if (!ocelotSegment.getTarget().getDisplayText().isEmpty()) {
 			ChangeTrack changeTrack = new ChangeTrack();
 			unit.setChangeTrack(changeTrack);
 			changeTrack.add(createRevisionsForTarget(okapiSegment
-			        .getTarget()));
-			targetVersions.add(new TargetVersion(TargetVersion.VERSION_PREFIX + "2"));
+			        .getTarget(), ocelotSegment.getSegmentId()));
+			targetVersions.put(ocelotSegment.getSegmentId(), new TargetVersion(TargetVersion.VERSION_PREFIX + "2"));
 		} else {
-			targetVersions.add(new TargetVersion(TargetVersion.VERSION_PREFIX + "1"));
+			targetVersions.put(ocelotSegment.getSegmentId(), new TargetVersion(TargetVersion.VERSION_PREFIX + "1"));
 		}
 	}
 	
 	
+	private boolean isRevisionsForTarget(Revisions revs, String ref) {
+
+		return revs.getAppliesTo().equals(Const.ELEM_TARGET)
+		        && ((ref != null && ref.equals(revs.getRef())) || (ref == null && revs
+		                .getRef() == null));
+	}
 	
-	private Revisions createRevisionsForTarget(Fragment target){
+	
+	private Revisions createRevisionsForTarget(Fragment target, String ref){
 		
 		Revisions revisions = new Revisions();
 		revisions.setAppliesTo(Const.ELEM_TARGET);
 		revisions.setCurrentVersion(TargetVersion.VERSION_PREFIX + "1");
+		revisions.setRef(ref);
 		Revision revision = new Revision();
 		revision.setDatetime(dateFormatter.format(new Date()));
 		revision.setVersion(TargetVersion.VERSION_PREFIX + "1");
@@ -313,7 +326,7 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 	 * @return Segment - Ocelot Segment
 	 * @throws MalformedURLException
 	 */
-    private OcelotSegment convertPartToSegment(net.sf.okapi.lib.xliff2.core.Segment unitPart, int segmentUnitPartIndex, List<Enrichment> sourceEnrichments, List<Enrichment> targetEnrichments, String unitId) throws MalformedURLException {
+    private OcelotSegment convertPartToSegment(net.sf.okapi.lib.xliff2.core.Segment unitPart, int segmentUnitPartIndex, List<Enrichment> sourceEnrichments, List<Enrichment> targetEnrichments, String unitId, String segId) throws MalformedURLException {
         segmentEventMapping.put(this.documentSegmentNum, this.events.size()-1);
         //TODO: load original target from file
 		OkapiSegment seg = new OkapiSegment.Builder()
@@ -322,6 +335,7 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 		        .source(new FragmentVariant(unitPart, false))
 				.target(new FragmentVariant(unitPart, true))
 				.tuId(unitId)
+				.segId(segId)
 				.build();
 		seg.addAllLQI(parseLqiData(unitPart));
 		seg.addAllProvenance(parseProvData(unitPart));
@@ -467,9 +481,12 @@ public class OkapiXLIFF20Parser implements XLIFFParser {
 	
 	public void updateTargetVersions(){
 		
-		for(TargetVersion tVersion: targetVersions){
+		for(TargetVersion tVersion: targetVersions.values()){
 			tVersion.nextVersion();
 		}
+//		for(TargetVersion tVersion: targetVersions){
+//			tVersion.nextVersion();
+//		}
 	}
 	
 	public SimpleDateFormat getRevisionDateFormatter(){
