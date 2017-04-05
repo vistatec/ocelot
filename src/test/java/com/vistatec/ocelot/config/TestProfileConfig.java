a62dba2885e8bb60256f8c53f68ac927186466ec
Marta Borriello <Marta.Borriello@vistatec.com>
2017-04-05 17:23:06 +0100

    log level changed to INFO

--- src/test/java/com/vistatec/ocelot/config/TestProfileConfig.java
+++ src/test/java/com/vistatec/ocelot/config/TestProfileConfig.java
@@ -2,6 +2,7 @@ package com.vistatec.ocelot.config;
 
 import java.io.File;
 import java.io.StringWriter;
+import java.net.URISyntaxException;
 
 import org.junit.Assert;
 import org.junit.Test;
@@ -12,18 +13,18 @@ import com.vistatec.ocelot.config.json.ProfileConfig;
 public class TestProfileConfig {
 
 	@Test
-	public void testReadProfile() throws TransferException{
+	public void testReadProfile() throws TransferException, URISyntaxException{
 		
-		File confFile = new File(getClass().getResource("def_profile_cfg.json").getFile());
+		File confFile = new File(getClass().getResource("def_profile_cfg.json").toURI());
 		ProfileConfigService confService = new ProfileConfigService(new ProfileConfigTransferService(confFile));
 		Assert.assertTrue(confService.mustPromptMessage());
 		Assert.assertEquals(ProfileConfig.DEFAULT_PROF_NAME, confService.getProfileName());
 	}
 	
 	@Test
-	public void testWriteProfile() throws TransferException {
+	public void testWriteProfile() throws TransferException, URISyntaxException {
 		
-		File confFile = new File(getClass().getResource("def_profile_cfg.json").getFile());
+		File confFile = new File(getClass().getResource("def_profile_cfg.json").toURI());
 		StringWriter writer = new StringWriter();
 		ProfileConfigService confService = new ProfileConfigService(new TestProfileConfigTransferService(confFile, writer));
 		confService.changeActiveProfile("Test");
